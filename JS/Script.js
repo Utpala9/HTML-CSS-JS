93c66ce730a24ea620d3364ec2f0b7fa69c2b9a3
Utpala9 <utpaladutta69@gmail.com>
2026-02-07 02:52:54 +0530

    Building js

---
+++ JS/Script.js
@@ -0,0 +1,10 @@
+// There is no boler plat
+
+// Basic normal text on console
+console.log("Are you all excited !")
+
+// warning text on console
+console.warn("This is warning")
+
+// error in console
+console.error("This is an error")
